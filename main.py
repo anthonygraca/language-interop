2bc73a1a7b5d19b958a79ed1acdec5e787e4be38
Anthony Graca <anthonygraca@protonmail.com>
2022-12-31 11:17:51 -0800

    added python example

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ import pathlib
 
 if __name__ == "__main__":
     # Load the shared library into ctypes
-    libname = pathlib.Path().absolute() / "libcmult.so"
-    c_lib = ctypes.CDLL(libname)
+    c_lib = ctypes.CDLL((pathlib.Path().absolute() / pathlib.Path("build/c")) / "libprint-from-c.so")
+    c_lib.PrintFromC()
+    cc_lib = ctypes.CDLL((pathlib.Path().absolute() / pathlib.Path("build/cc")) / "libprint-from-cpp.so")
+    cc_lib.PrintFromCpp()
+    print("Hello world from python!")
